9e3371343eddf5e646c8ba2d35e3719a46d433d4
aditya-gupta-dev <adityagupta2087@gmail.com>
2025-10-26 17:29:36 +0530

    completed problem 14

--- 13/main.go
+++ 13/main.go
@@ -1,5 +1,25 @@
 package main
 
+func romanToInt(s string) int {
+	var result int
+	romans := map[byte]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+	for i := 0; i < len(s); i++ {
+		if i+1 < len(s) && romans[s[i]] < romans[s[i+1]] {
+			result -= romans[s[i]]
+		} else {
+			result += romans[s[i]]
+		}
+	}
+	return result
+}
 func main() {
-
+	println(romanToInt("MCMXCIV"))
 }
