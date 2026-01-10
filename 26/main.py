f1b74ef8d95249e4205876874f3efc3d20f93719
Aditya Gupta <adityagupta2087@gmail.com>
2026-01-10 23:36:06 +0530

    completed problem 26

---
+++ 26/main.py
@@ -0,0 +1,19 @@
+def removeDuplicates(nums: list[int]) -> int:
+    if not nums:
+        return 0
+    
+    k = 1  
+    
+    for i in range(1, len(nums)):
+        if nums[i] != nums[k - 1]:  
+            nums[k] = nums[i] 
+            k += 1
+    
+    return k
+
+
+n = [1,1,2]
+
+k = removeDuplicates(n)
+
+print(k, len(n), n)
